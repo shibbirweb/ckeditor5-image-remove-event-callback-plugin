62ba68d0115e0cd28efa8f015f8537180a694330
MD. Shibbir Ahmed <shibbirweb@gmail.com>
2022-03-22 22:19:55 +0600

    Add demo project

---
+++ demo/build/translations/af.js
@@ -0,0 +1,1 @@
+!function(e){const n=e.af=e.af||{};n.dictionary=Object.assign(n.dictionary||{},{"%0 of %1":"","Block quote":"Blok-aanhaling",Bold:"Vetgedruk",Cancel:"Kanselleer","Cannot upload file:":"Lêer nie opgelaai nie:",Italic:"Skuinsgedruk",Save:"Berg","Show more items":""}),n.getPluralForm=function(e){return 1!=e}}(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
